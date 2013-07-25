4dd0a28dbe8b4395178c756e812d0d09ba9fb26b
Andrew <abeinstein@uchicago.edu>
2013-07-25 01:54:02 -0500

    Added left table view

---
+++ recoil/recoil/BGCLeftTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BGCLeftTableViewController.h
+//  recoil
+//
+//  Created by Andrew Beinstein on 7/25/13.
+//  Copyright (c) 2013 BGC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGCLeftTableViewController : UITableViewController
+
+@end
---
+++ recoil/recoil/BGCSidebarViewController.h
@@ -0,0 +1,14 @@
+//
+//  BGCSidebarViewController.h
+//  recoil
+//
+//  Created by Andrew Beinstein on 7/25/13.
+//  Copyright (c) 2013 BGC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "JASidePanelController.h"
+
+@interface BGCSidebarViewController : JASidePanelController
+
+@end
