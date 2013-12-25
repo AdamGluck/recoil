27965b868adfd995a9f9ed4b7aafbd377adc156f
AdamGluck <adamt.gluck@gmail.com>
2013-12-25 13:10:21 -0500

    set up the settings view

--- recoil/recoil/BGCSettingsViewController.m
+++ recoil/recoil/BGCSettingsViewController.m
@@ -46,7 +46,7 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return 3;
+    return 4;
 }
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
@@ -54,6 +54,9 @@
         UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"About" message:@"\n Recoil is a homicide tracker that encourages users to make a difference by getting involved in local organizations and spreading awareness of Chicago gun violence." delegate:self cancelButtonTitle:@"Okay" otherButtonTitles: nil];
         [alert show];
     } else if (indexPath.row == 1){
+        UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Push Notifications" message:@"You can change push notifications in the Settings -> Notification Center by selecting Recoil under the section labeled include." delegate:self cancelButtonTitle:@"Okay" otherButtonTitles: nil];
+        [alert show];
+    } else if (indexPath.row == 2){
         UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Terms of Service" message:@"1. Recoil is not affiliated, associated, authorized, endorsed by, or in any way officially connected with organizations listed within “make a difference” section. You expressly agree that use of the service is at your sole risk. The official Recoil web site is available at www.recoil-app.com. \n \n 2. While Recoil uses reasonable efforts to include accurate and up-to-date information, we make no warranties or representations as to the accuracy of the content and assume no liability or responsibility for any error or omission in the content. You expressly agree that use of the service is at your sole risk." delegate:self cancelButtonTitle:@"Okay" otherButtonTitles: nil];
         [alert show];
     } else {
@@ -65,6 +68,8 @@
         controller.delegate = self;
         [self presentViewController:controller animated:YES completion:nil];
     }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
 }
 
 - (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error
