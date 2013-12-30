645c5dcc009e385307ce0233c71258a65c97f8ba
AdamGluck <adamt.gluck@gmail.com>
2013-12-30 00:45:48 -0500

    hopefully this fixes the background regenerating thing

--- recoil/recoil/BGCAppDelegate.m
+++ recoil/recoil/BGCAppDelegate.m
@@ -72,11 +72,7 @@ didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)newDeviceToken {
 - (void)application:(UIApplication *)application
 didReceiveRemoteNotification:(NSDictionary *)userInfo {
     [PFPush handlePush:userInfo];
-
-    if (application.applicationState == UIApplicationStateActive ){
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"pushNotification" object:nil userInfo:userInfo];
-    }
-    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"pushNotification" object:nil userInfo:userInfo];    
 }
 
 @end
